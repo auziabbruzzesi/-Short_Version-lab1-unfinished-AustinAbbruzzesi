c748540ad355d6658da182df151d233df6521994
auziabbruzzesi <austinta@udel.edu>
2016-02-22 10:34:07 -0500

    Potential Master- everything works

--- lab1(2)/src/main_package/Main.java
+++ lab1(2)/src/main_package/Main.java
@@ -1,20 +1,22 @@
 package main_package;
 
 public class Main {
-	private static double ret_budget = Query.ask_for_input("please input ret_budget:");//$10,000
-	private static double ret_ssi = Query.ask_for_input("please input ret_ssi:");//$2,642
-	private static double inv_safe = Query.ask_for_input("please input inv_safe:");//2%
-	private static double ret_term = Query.ask_for_input("pleast input ret_term:");//20
-	private static double inv_risky = Query.ask_for_input("please input inv risky");//7%
+	private static double ret_budget = Query.ask_for_input("please input monthly budget post-retirement:");//$10,000
+	private static double ret_ssi = Query.ask_for_input("please input social security income:");//$2,642
+	private static double inv_safe = Query.ask_for_input("please input annual return post-retirement:");//2%
+	private static double ret_term = Query.ask_for_input("pleast input # of years retired:");//20
+	private static double inv_risky = Query.ask_for_input("please input annual return pre-retirement:");//7%
 	private static double lump_sum = Post.calc_post(ret_budget, ret_ssi, inv_safe, ret_term);//PGV
 	private static double emp_term = Query.ask_for_input("please enter # years worked:");//40
 	private static double emp_save = Pre.calc_emp_save(inv_risky, lump_sum, emp_term);//PGV
 
 	public static void main(String[] args) {
-		System.out.print("lump sum:");
-		System.out.println(lump_sum);
-		System.out.print("emp_save:");
-		System.out.print(emp_save);
+		System.out.print("What you need saved:");
+		System.out.printf("%.2f",lump_sum);
+		System.out.println("");
+		System.out.print("Amount to save each month:");
+		System.out.printf("%.2f",emp_save);
+		Query.user_input.close();
 		
 		}
 
--- lab1(2)/src/main_package/Query.java
+++ lab1(2)/src/main_package/Query.java
@@ -1,10 +1,8 @@
 package main_package;
 import java.util.*;
 public class Query extends Main{
+	static Scanner user_input = new Scanner(System.in);
 	public static double ask_for_input(String request){
-		@SuppressWarnings("resource")
-		Scanner user_input = new Scanner(System.in);
-		
 		System.out.print(request);
 		double value = user_input.nextInt();
 		return value;
